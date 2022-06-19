8bcf6ff6474da942508466ae542286811de1f2ba
seyha30 <57175099+seyha30@users.noreply.github.com>
2022-06-19 20:48:32 +0700

    add categorize

--- source/merge.py
+++ source/merge.py
@@ -1,4 +1,5 @@
 from importlib.resources import path
+from operator import index
 import pandas as pd
 import numpy as np
 import seaborn as sbn
@@ -11,6 +12,11 @@ file = open('winequality-both.csv','w+')
 file.close()
 for file_name in all_files:
     data_frame = pd.read_csv(file_name,sep=',',header=0)
-    print(file_name)
+    # print(file_name)
+    type = file_name.split('-')[1].split('.')[0]
+    if type == 'red':
+        data_frame.insert(0,'type','red')
+    if type == 'white':
+        data_frame.insert(0,'type','white')
     data_frame.to_csv('winequality-both.csv',mode='a',index=False)
     # print(data_frame.describe())
