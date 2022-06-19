448e54a3ff450d49c93a26b6ab6e3ab95158eb66
seyha30 <57175099+seyha30@users.noreply.github.com>
2022-06-19 21:58:18 +0700

    m

--- source/merge.py
+++ source/merge.py
@@ -1,22 +1,17 @@
-from importlib.resources import path
-from operator import index
 import pandas as pd
-import numpy as np
-import seaborn as sbn
 import glob as glb
 import os
-from matplotlib import pyplot as plt
 all_files = glb.glob(os.path.join('../','*.csv'))
 # clear previous file content
 file = open('winequality-both.csv','w+')
 file.close()
 for file_name in all_files:
     data_frame = pd.read_csv(file_name,sep=',',header=0)
-    # print(file_name)
+    # split wine type
     type = file_name.split('-')[1].split('.')[0]
     if type == 'red':
-        data_frame.insert(0,'type','red')
+        data_frame.insert(0,'type',type)
     if type == 'white':
-        data_frame.insert(0,'type','white')
+        data_frame.insert(0,'type',type)
+    # write to csv
     data_frame.to_csv('winequality-both.csv',mode='a',index=False)
-    # print(data_frame.describe())
