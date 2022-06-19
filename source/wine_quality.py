883b5415749f4c42d2e87c479c93f21fbbc14977
seyha30 <57175099+seyha30@users.noreply.github.com>
2022-06-19 15:55:37 +0700

    merg data

--- source/wine_quality.py
+++ source/wine_quality.py
@@ -1,5 +1,11 @@
+from importlib.resources import path
 import pandas as pd
 import numpy as np
 import seaborn as sbn
-
-print('hello')
+import glob as glb
+import os
+from matplotlib import pyplot as plt
+all_files = glb.glob(os.path.join('../','*.csv'))
+for file_name in all_files:
+    data_frame = pd.read_csv(file_name,sep=',',header=0)
+    data_frame.to_csv('winequality-both.csv',index=False)
