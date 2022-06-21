f6660f1cd2434375f1c9b7e72ec4c1ced1bc150c
seyha30 <57175099+seyha30@users.noreply.github.com>
2022-06-21 22:25:04 +0700

    123

--- source/merge.py
+++ source/merge.py
@@ -5,13 +5,16 @@ all_files = glb.glob(os.path.join('../','*.csv'))
 # clear previous file content
 file = open('winequality-both.csv','w+')
 file.close()
+temporary_data_frame = []
 for file_name in all_files:
-    data_frame = pd.read_csv(file_name,sep=',',header=0)
+    data_frame = pd.read_csv(file_name)
     # split wine type
     type = file_name.split('-')[1].split('.')[0]
     if type == 'red':
         data_frame.insert(0,'type',type)
     if type == 'white':
         data_frame.insert(0,'type',type)
+    temporary_data_frame.append(data_frame)
     # write to csv
-    data_frame.to_csv('winequality-both.csv',mode='a',index=False)
+df = pd.concat(temporary_data_frame)
+df.to_csv('winequality-both.csv',mode='a',index=False)
